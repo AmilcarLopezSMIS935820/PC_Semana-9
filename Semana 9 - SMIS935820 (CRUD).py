6853e73327abe28d269d27e28ada754c4f467c28
Am_Soft10 <80600624+AmilcarLopezSMIS935820@users.noreply.github.com>
2021-09-24 22:17:55 -0600

    Update Semana 9 - SMIS935820 (CRUD).py

--- Semana 9 - SMIS935820 (CRUD).py
+++ Semana 9 - SMIS935820 (CRUD).py
@@ -3,13 +3,14 @@ import sqlite3 #Importando Librería de SQL Lite
 conexion = sqlite3.connect("ControlVacunas.db") #Abriendo la conexión con BD
 
 #Actualizando un registro
-conexion.execute("UPDATE vacunas set nombre='Katerin', apellido='Molina', edad=20, sexo='Mujer', telefono='4591-1268', direccion='San Salvador, San Salvador', vacunas=2 where id_persona=6")
+conexion.execute("UPDATE vacunas set nombre='Katerin', apellido='Molina', edad=20, sexo='Mujer', telefono='4591-1268', direccion='San Salvador, San Salvador', vacunas=2 WHERE id_persona=6")
 
 conexion.commit()
 
 #Borrando un registro
-conexion.execute("DELETE from vacunas where id_persona=6;")
-conexion.commit()
+conexion.execute("DELETE FROM vacunas WHERE id_persona=6;")
+conexion.commit()
+
 
 #Cerrando conexión a BD
 conexion.close()
