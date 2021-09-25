25b6bd6c24acc5280404763efc26ece2c5f404b2
Am_Soft10 <80600624+AmilcarLopezSMIS935820@users.noreply.github.com>
2021-09-24 21:56:12 -0600

    Más funciones CRUD para la BD

---
+++ Semana 9 - SMIS935820 (CRUD).py
@@ -0,0 +1,15 @@
+import sqlite3 #Importando Librería de SQL Lite
+
+conexion = sqlite3.connect("ControlVacunas.db") #Abriendo la conexión con BD
+
+#Actualizando un registro
+conexion.execute("UPDATE vacunas set nombre='Katerin', apellido='Molina', edad=20, sexo='Mujer', telefono='4591-1268', direccion='San Salvador, San Salvador', vacunas=2 where id_persona=6")
+
+conexion.commit()
+
+#Borrando un registro
+conexion.execute("DELETE from vacunas where id_persona=6;")
+conexion.commit()
+
+#Cerrando conexión a BD
+conexion.close()
